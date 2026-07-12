e33f202488bca3af1884af1e34e705deb703c027
Akhil T <fslearn@vc.com>
2026-07-12 23:30:25 +0530

    fix: restore missing api prefix to backend base url string

--- nexus-ems-frontend/src/api/axios.js
+++ nexus-ems-frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseDomain = import.meta.env.VITE_API_BASE_URL;
+
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL
+  baseURL: `${baseDomain}/api`
 });
 
 API.interceptors.request.use((config) => {
